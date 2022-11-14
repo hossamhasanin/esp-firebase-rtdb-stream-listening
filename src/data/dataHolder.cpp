28509bd8ced977ae9f88f3e1388a25231974724a
hossamhasanin <kokojake13@gmail.com>
2022-11-14 18:00:46 +0200

    fix bug in data holder

--- src/data/dataHolder.cpp
+++ src/data/dataHolder.cpp
@@ -8,15 +8,16 @@ DataItem DataHolder::parseDataFromKeyValue(const char* key, const char* value) {
     int keyInt = atoi(key);
     DataItem changedDataItem;
     
+    uint8_t valueByte = (uint8_t)atoi(value);
     // compare the key with 0
-    if (keyInt == temprature) {
+    if (keyInt == temprature && _data[temprature] != valueByte) {
         // convert the value to uint8_t
-        _data[temprature] = (uint8_t)atoi(value);
+        _data[temprature] = valueByte;
         changedDataItem.key = temprature;
         changedDataItem.value = _data[temprature];
-    } else if (keyInt == switch1) {
+    } else if (keyInt == switch1 && _data[switch1] != valueByte) {
         // convert the value to uint8_t
-        _data[switch1] = (uint8_t)atoi(value);
+        _data[switch1] = valueByte;
         changedDataItem.key = switch1;
         changedDataItem.value = _data[switch1];
     } else {
